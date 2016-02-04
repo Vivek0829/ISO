fc1098b27f3560acac38651b45a8a009aea6bdd4
Vivek0829 <vivek0829@gmail.com>
2016-02-04 00:57:45 -0500

    Update tomcat.bat
    Tomcat file has been added to the list

--- tomcat.bat
+++ tomcat.bat
@@ -1,2 +1,2 @@
 cd Sner
-java -mx1000m -cp stanford-ner.jar edu.stanford.nlp.ie.NERServer -loadClassifier classifiers/english.all.3class.distsim.crf.ser.gz -port 8080 -outputFormat inlineXML
+java -mx1000m -cp stanford-ner.jar edu.stanford.nlp.ie.NERServer -loadClassifier classifiers/english.muc.7class.distsim.crf.ser.gz -port 8080 -outputFormat inlineXML

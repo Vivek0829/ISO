ae9dfdb334ea2e9b73871e63f9630906c0a5715c
Vivek0829 <vivek0829@gmail.com>
2016-02-04 00:59:54 -0500

    Update tomcat.bat

--- tomcat.bat
+++ tomcat.bat
@@ -1,2 +1,2 @@
-cd Sner
+cd StanfordNER # Change the current directory to the Stanford ner directory
 java -mx1000m -cp stanford-ner.jar edu.stanford.nlp.ie.NERServer -loadClassifier classifiers/english.muc.7class.distsim.crf.ser.gz -port 8080 -outputFormat inlineXML
